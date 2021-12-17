32b584cbfb77a1ccb42021b930c5cff81d63a6e8
rizwanishaq <rizwanishaq@gmail.com>
2021-12-17 18:52:30 +0100

    almost finished

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Profile from "./pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <>
@@ -28,6 +30,11 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
