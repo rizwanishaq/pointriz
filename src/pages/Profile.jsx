fd415858b50680b1f3dc6f86261204c95600596d
rizwanishaq <rizwanishaq@gmail.com>
2021-12-20 17:39:35 +0100

    completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -95,6 +95,10 @@ const Profile = () => {
     }
   };
 
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/${listingId}`);
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -152,6 +156,7 @@ const Profile = () => {
                   key={listing.id}
                   listing={listing.data}
                   id={listing.id}
+                  onEdit={() => onEdit(listing.id)}
                   onDelete={() => onDelete(listing.id)}
                 />
               ))}
